d3a8b17675e08f76b071a5ae3edfc4dab0d85467
Kletus Stern <sternwork@gmx.de>
2025-12-18 00:22:00 +0100

    Created new packages: curves

---
+++ curves/__init__.py
@@ -0,0 +1,10 @@
+from .plane_curves import ProjectivePlaneCurve, PPC_TangentCone, ProjectiveFlag
+from .plane_curves_valued import PlaneCurveOverValuedField, PlaneModel
+
+__all__ = [
+    'ProjectivePlaneCurve',
+    'PPC_TangentCone',
+    'ProjectiveFlag',
+    'PlaneCurveOverValuedField',
+    'PlaneModel'
+]
