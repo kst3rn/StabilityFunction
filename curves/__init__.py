921b53ec9b13591dd24fe1cdae55d84ea5cb5adc
Kletus Stern <sternwork@gmx.de>
2025-12-18 00:35:34 +0100

    changed tab size

--- curves/__init__.py
+++ curves/__init__.py
@@ -2,9 +2,9 @@ from .plane_curves import ProjectivePlaneCurve, PPC_TangentCone, ProjectiveFlag
 from .plane_curves_valued import PlaneCurveOverValuedField, PlaneModel
 
 __all__ = [
-    'ProjectivePlaneCurve',
-    'PPC_TangentCone',
-    'ProjectiveFlag',
-    'PlaneCurveOverValuedField',
-    'PlaneModel'
+  'ProjectivePlaneCurve',
+  'PPC_TangentCone',
+  'ProjectiveFlag',
+  'PlaneCurveOverValuedField',
+  'PlaneModel'
 ]
