9660f821a88077e928c8fae760b80c2bb0a1dc09
Kletus Stern <sternwork@gmx.de>
2025-12-18 03:18:42 +0100

    Changed tab size

--- stability/__init__.py
+++ stability/__init__.py
@@ -1,10 +1,10 @@
 # stability/__init__.py
 
 from .admissible_functions import (
-    AdmissibleFunction, 
-    ValuativeFunction, 
-    MinimumOfValuativeFunctions,
-    valuative_function
+  AdmissibleFunction, 
+  ValuativeFunction, 
+  MinimumOfValuativeFunctions,
+  valuative_function
 )
 from .stability_function import StabilityFunction, BTB_Point
 from .parametric_optimization import minimum_as_valuative_function
