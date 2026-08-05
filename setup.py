7771ef62fb97fc76fa842061696e151529d36e21
Kletus Stern <sternwork@gmx.de>
2026-08-05 20:25:29 +0200

    Updated setup file.

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="semistable_model",
     version="1.0",
+    author="Kletus Stern, Stefan Wewers"
+    author_email="sternwork@gmx.de, stefan.wewers@uni-ulm.de"
     packages=find_packages(),
 )
